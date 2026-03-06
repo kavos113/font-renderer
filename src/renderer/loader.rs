42ae68004a0343c5452e0deb27fe123b0f03ee32
kavos113 <kohei.hadano@gmail.com>
2026-03-06 21:03:30 +0900

    loca

--- src/renderer/loader.rs
+++ src/renderer/loader.rs
@@ -1,13 +1,14 @@
 use crate::ttf::glyph::Glyph;
-use crate::ttf::table::{HeadTable, MaxpTable};
+use crate::ttf::table::{HeadTable, MaxpTable, MaxpTable1_0};
 use crate::ttf::table_directory::TTFTableDirectory;
 use crate::ttf::types::{Reader, Tag};
 
 pub struct Font<'a> {
     data: &'a [u8],
     reader: Reader<'a>,
+    directory: TTFTableDirectory,
     head: HeadTable,
-    maxp: MaxpTable,
+    maxp: MaxpTable1_0,
     loca: Vec<u32>,
     glyf: Vec<Glyph>,
 }
@@ -32,13 +33,41 @@ impl Font<'_> {
         r.seek(maxp_record.offset as usize);
         let maxp = MaxpTable::read_from(&mut r);
 
+        let maxp_1 = match maxp {
+            MaxpTable::Version0_5(_) => {
+                panic!("TrueType fonts with 'maxp' version 0.5 are not supported")
+            }
+            MaxpTable::Version1_0(table) => table,
+        };
+
         Font {
             data,
             reader: r,
+            directory: table_directory,
             head,
-            maxp,
+            maxp: maxp_1,
             loca: vec![],
             glyf: vec![],
         }
+    }
+
+    fn load_loca(&mut self) {
+        let loca_record = self
+            .directory
+            .get_table_record(&Tag::from_str("loca"))
+            .expect("Failed to find 'loca' table");
+
+        self.reader.seek(loca_record.offset as usize);
+
+        let num_glyphs = self.maxp.num_glyphs as usize;
+        self.loca = if self.head.index_to_loc_format == 0 {
+            (0..=num_glyphs)
+                .map(|_| self.reader.read_uint16() as u32 * 2)
+                .collect()
+        } else {
+            (0..=num_glyphs)
+                .map(|_| self.reader.read_uint32())
+                .collect()
+        };
     }
 }
--- src/ttf/table.rs
+++ src/ttf/table.rs
@@ -49,26 +49,26 @@ impl HeadTable {
 }
 
 pub struct MaxpTable0_5 {
-    version: Version16Dot16,
-    num_glyphs: uint16,
+    pub version: Version16Dot16,
+    pub num_glyphs: uint16,
 }
 
 pub struct MaxpTable1_0 {
-    version: Version16Dot16,
-    num_glyphs: uint16,
-    max_points: uint16,
-    max_contours: uint16,
-    max_composite_points: uint16,
-    max_composite_contours: uint16,
-    max_zones: uint16,
-    max_twilight_points: uint16,
-    max_storage: uint16,
-    max_function_defs: uint16,
-    max_instruction_defs: uint16,
-    max_stack_elements: uint16,
-    max_size_of_instructions: uint16,
-    max_component_elements: uint16,
-    max_component_depth: uint16,
+    pub version: Version16Dot16,
+    pub num_glyphs: uint16,
+    pub max_points: uint16,
+    pub max_contours: uint16,
+    pub max_composite_points: uint16,
+    pub max_composite_contours: uint16,
+    pub max_zones: uint16,
+    pub max_twilight_points: uint16,
+    pub max_storage: uint16,
+    pub max_function_defs: uint16,
+    pub max_instruction_defs: uint16,
+    pub max_stack_elements: uint16,
+    pub max_size_of_instructions: uint16,
+    pub max_component_elements: uint16,
+    pub max_component_depth: uint16,
 }
 
 pub enum MaxpTable {
